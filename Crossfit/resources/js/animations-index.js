bf875ac50fce2a2698489f0b1b852827e64627b7
LuisDavidNietoMejia <luisdavidnietomejia@gmail.com>
2019-09-15 13:14:58 -0300

    finalized changes javascript

--- Crossfit/resources/js/animations-index.js
+++ Crossfit/resources/js/animations-index.js
@@ -1,198 +1,151 @@
+$(window).scroll(function () {
 
-alert("La resolución de tu pantalla es: " + screen.width + " x " + screen.height) 
+  $('#block-1').each(function () {
+    animationScrollLeft('#block-1');
+  });
 
+  $('#block-2').each(function () {
+    animationScrollLeft('#block-2');
+  });
 
-   $("#button-email-header").click(function () {
+  $('#block-3').each(function () {
+    animationScrollLeft('#block-3');
+  });
 
-      var empty = $('#email-input-header').val();
+  $('#block-7').each(function () {
+    animationScroll('#block-7');
+  });
 
-      if (empty != "") {
-        $('#block-contact-2').each(function () {
-          $('#block-contact-1').hide();
-          $("#block-contact-2").removeClass("visibility-contact-none");
-          animationScrollLeft('#block-contact-2');
-        });
-      } else {
-        alert('Campo email vacio!')
-      }
+  $('#block-8').each(function () {
+    animationScroll('#block-8');
+  });
 
-    });
+  $('#block-9').each(function () {
+    animationScrollLeft('#block-9');
+  });
 
-    $("#button-name-header").click(function () {
-
-      var empty = $('#name-input-header').val();
-      
-      if (empty != "") {
-        $('#block-contact-3').each(function () {
-        $('#block-contact-2').hide();
-        $("#block-contact-3").removeClass("visibility-contact-none");
-        animationScrollLeft('#block-contact-3');
- 
-       var width = screen.width; 
-      //  var height = screen.height;
-        
-       if(width < 769){
-        $("#button-box-phone").removeClass("visibility-contact-none");
-        animationScrollLeft('#button-box-phone');
-       }
-
-      });
-      } else {
-        alert('Campo nombre vacio!')
-      }
-
-      
-    });
-
-    $(window).scroll(function () {
-
-      $('#block-1').each(function () {
-        animationScrollLeft('#block-1');
-      });
-
-      $('#block-2').each(function () {
-        animationScrollLeft('#block-2');
-      });
-
-      $('#block-3').each(function () {
-        animationScrollLeft('#block-3');
-      });
-
-      $('#block-7').each(function () {
-        animationScroll('#block-7');
-      });
-
-      $('#block-8').each(function () {
-        animationScroll('#block-8');
-      });
-
-      $('#block-9').each(function () {
-        animationScrollLeft('#block-9');
-      });
-    
-      $('#block-12').each(function () {
-        animationScrollLeft('#block-12');
-      });
-
-      
-      $('#block-14').each(function () {
-        animationScrollLeft('#block-14');
-      });
-
-      $('#block-15').each(function () {
-        animationScrollLeft('#block-15');
-      });
-
-      $('#block-16').each(function () {
-        animationScrollLeft('#block-16');
-      });
-
-      $('#block-17').each(function () {
-        animationScrollRight('#block-17');
-      });
-
-      $('#block-18').each(function () {
-        animationScrollLeft('#block-18');
-      });
-
-      $('#block-19').each(function () {
-        animationScrollRight('#block-19');
-      });
-
-      $('#block-20').each(function () {
-        animationScrollLeft('#block-20');
-      });
-
-      $('#block-21').each(function () {
-        animationScrollRight('#block-21');
-      });
-
-      $('#block-22').each(function () {
-        animationScrollLeft('#block-22');
-      });
-
-      $('#block-23').each(function () {
-        animationScrollRight('#block-23');
-      });
-     
-      $('#block-26').each(function () {
-        animationScrollLeft('#block-26');
-      });
-
-      $('#block-28').each(function () {
-        animationScrollLeft('#block-28');
-      });
-
-      $('#block-29').each(function () {
-        animationScrollLeft('#block-29');
-      });
-
-      $('#block-30').each(function () {
-        animationScrollLeft('#block-30');
-      });
-
-      $('#block-31').each(function () {
-        animationScrollLeft('#block-31');
-      });
-
-      $('#block-32').each(function () {
-        animationScrollLeft('#block-32');
-      });
-
-      $('#block-33').each(function () {
-        animationScrollLeft('#block-33');
-      });
-
-      $('#block-34').each(function () {
-        animationScrollLeft('#block-34');
-      });
-
-      $('#block-35').each(function () {
-        animationScrollLeft('#block-35');
-      });
-
-    });
+  $('#block-12').each(function () {
+    animationScrollLeft('#block-12');
+  });
 
 
-    function animationScroll(id) {
+  $('#block-14').each(function () {
+    animationScrollLeft('#block-14');
+  });
 
-      var topOfWindow = $(window).scrollTop(),
-        bottomOfWindow = topOfWindow + $(window).height();
-      var imagePos = $(id).offset().top;
-      if (imagePos <= bottomOfWindow - 100 && imagePos >= topOfWindow - 250) {
+  $('#block-15').each(function () {
+    animationScrollLeft('#block-15');
+  });
 
-        $(id).addClass('slideUp');
-      } else {
-        $(id).removeClass('slideUp');
-      }
+  $('#block-16').each(function () {
+    animationScrollLeft('#block-16');
+  });
 
-    }
+  $('#block-17').each(function () {
+    animationScrollRight('#block-17');
+  });
 
-    function animationScrollLeft(id) {
+  $('#block-18').each(function () {
+    animationScrollLeft('#block-18');
+  });
 
-      var topOfWindow = $(window).scrollTop(),
-        bottomOfWindow = topOfWindow + $(window).height();
-      var imagePos = $(id).offset().top;
-      if (imagePos <= bottomOfWindow - 100 && imagePos >= topOfWindow - 250) {
+  $('#block-19').each(function () {
+    animationScrollRight('#block-19');
+  });
 
-        $(id).addClass('slideLeft');
-      } else {
-        $(id).removeClass('slideLeft');
-      }
+  $('#block-20').each(function () {
+    animationScrollLeft('#block-20');
+  });
 
-    }
+  $('#block-21').each(function () {
+    animationScrollRight('#block-21');
+  });
 
-    function animationScrollRight(id) {
+  $('#block-22').each(function () {
+    animationScrollLeft('#block-22');
+  });
 
-      var topOfWindow = $(window).scrollTop(),
-        bottomOfWindow = topOfWindow + $(window).height();
-      var imagePos = $(id).offset().top;
-      if (imagePos <= bottomOfWindow - 100 && imagePos >= topOfWindow - 250) {
+  $('#block-23').each(function () {
+    animationScrollRight('#block-23');
+  });
 
-        $(id).addClass('slideRight');
-      } else {
-        $(id).removeClass('slideRight');
-      }
+  $('#block-26').each(function () {
+    animationScrollLeft('#block-26');
+  });
 
-    }
-  
+  $('#block-28').each(function () {
+    animationScrollLeft('#block-28');
+  });
+
+  $('#block-29').each(function () {
+    animationScrollLeft('#block-29');
+  });
+
+  $('#block-30').each(function () {
+    animationScrollLeft('#block-30');
+  });
+
+  $('#block-31').each(function () {
+    animationScrollLeft('#block-31');
+  });
+
+  $('#block-32').each(function () {
+    animationScrollLeft('#block-32');
+  });
+
+  $('#block-33').each(function () {
+    animationScrollLeft('#block-33');
+  });
+
+  $('#block-34').each(function () {
+    animationScrollLeft('#block-34');
+  });
+
+  $('#block-35').each(function () {
+    animationScrollLeft('#block-35');
+  });
+
+});
+
+
+function animationScroll(id) {
+
+  var topOfWindow = $(window).scrollTop(),
+    bottomOfWindow = topOfWindow + $(window).height();
+  var imagePos = $(id).offset().top;
+  if (imagePos <= bottomOfWindow - 100 && imagePos >= topOfWindow - 250) {
+
+    $(id).addClass('slideUp');
+  } else {
+    $(id).removeClass('slideUp');
+  }
+
+}
+
+function animationScrollLeft(id) {
+
+  var topOfWindow = $(window).scrollTop(),
+    bottomOfWindow = topOfWindow + $(window).height();
+  var imagePos = $(id).offset().top;
+  if (imagePos <= bottomOfWindow - 100 && imagePos >= topOfWindow - 250) {
+
+    $(id).addClass('slideLeft');
+  } else {
+    $(id).removeClass('slideLeft');
+  }
+
+}
+
+function animationScrollRight(id) {
+
+  var topOfWindow = $(window).scrollTop(),
+    bottomOfWindow = topOfWindow + $(window).height();
+  var imagePos = $(id).offset().top;
+  if (imagePos <= bottomOfWindow - 100 && imagePos >= topOfWindow - 250) {
+
+    $(id).addClass('slideRight');
+  } else {
+    $(id).removeClass('slideRight');
+  }
+
+}
---
+++ Crossfit/resources/js/validate-index.js
@@ -0,0 +1,241 @@
+//formulario contacto footer:
+
+$("#button-contact-form-info").click(function () {
+
+    var email = $('#input-contact-form-email').val();
+    var name = $('#input-contact-form-name').val();
+    var phone = $('#input-contact-form-phone').val();
+
+    if (name == "") {
+        alert('Campo name vacio!')
+        return;
+    }
+    if (email == "") {
+        alert('Campo email vacio!')
+        return;
+    }
+    if (phone == "") {
+        alert('Campo telefono vacio!')
+        return;
+    }
+
+    if (validateEmail(email)) {
+        if (validateString(name, 'nombre')) {
+            if (validatePhone(phone)) {
+                alert('llamar operacion asincrona: ajax o axios;')
+                alert('data: email: ' + email + ', nombre: ' + name + ', phone: ' + phone)
+            }
+        }
+    }
+
+});
+
+
+// alert("La resolución de tu pantalla es: " + screen.width + " x " + screen.height);
+
+//formulario contacto header:
+$("#button-email-header").click(function () {
+
+    var email = $('#email-input-header').val();
+
+    if (email != "") {
+        $('#block-contact-2').each(function () {
+
+            if (validateEmail(email)) {
+                $('#block-contact-1').hide();
+                $("#block-contact-2").removeClass("visibility-contact-none");
+                animationScrollLeft('#block-contact-2');
+            }
+        });
+    } else {
+        alert('Campo email vacio!')
+    }
+
+});
+
+$("#button-name-header").click(function () {
+
+    var name = $('#name-input-header').val();
+
+    if (name != "") {
+
+        if (validateString(name, 'nombre')) {
+
+            $('#block-contact-3').each(function () {
+                $('#block-contact-2').hide();
+                $("#block-contact-3").removeClass("visibility-contact-none");
+                animationScrollLeft('#block-contact-3');
+
+                var width = screen.width;
+
+                if (width < 769) {
+                    $("#block-contact-3").removeClass("input-group");
+                    $("#button-box-header-box").removeClass("visibility-contact-none");
+                    animationScrollLeft('#button-box-header-box');
+                }
+
+            });
+        }
+    } else {
+        alert('Campo nombre vacio!')
+    }
+
+});
+
+//enviar pantallas pequeñas:
+
+$("#button-box-header-box").click(function () {
+
+    var email = $('#email-input-header').val();
+    var name = $('#name-input-header').val();
+    var box = $('#box-input-header').val();
+
+    if (box != "") {
+        //llamada a ajax o axios;        
+        alert('llamar operacion asincrona: ajax o axios;')
+        alert('data: email: ' + email + ', nombre: ' + name + ', box: ' + box)
+
+        let valoresCheck = [];
+
+        $("input[type=checkbox]:checked").each(function () {
+            if (this.value != '') {
+                valoresCheck.push(this.value);
+            }
+        });
+
+        for (i in valoresCheck) {
+            alert('checkbox: ' + valoresCheck[i]);
+        }
+
+
+    } else {
+        alert('Campo box vacio!')
+    }
+
+});
+
+// enviar pantallas grandes:
+$("#button-box-header").click(function () {
+
+    var email = $('#email-input-header').val();
+    var name = $('#name-input-header').val();
+    var box = $('#box-input-header').val();
+
+    if (box != "") {
+        //llamada a ajax o axios;        
+        alert('llamar operacion asincrona: ajax o axios;')
+        alert('data: email: ' + email + ', nombre: ' + name + ', box: ' + box)
+
+        let valoresCheck = [];
+
+        $("input[type=checkbox]:checked").each(function () {
+            if (this.value != '') {
+                valoresCheck.push(this.value);
+            }
+        });
+
+        for (i in valoresCheck) {
+            alert('checkbox: ' + valoresCheck[i]);
+        }
+
+
+    } else {
+        alert('Campo box vacio!')
+    }
+
+});
+
+function validateEmail(email) {
+
+    // var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
+    // var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+    // var regex2 = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    var regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if (regex.test(email)) {
+        return true;
+
+    } else {
+        alert('Correo no valido');
+        return false;
+    }
+
+}
+
+function validateString(string, input) {
+
+    var regex = /^[a-zA-Z]{4,}$/;
+
+    if (regex.test(string)) {
+        return true;
+
+    } else {
+        alert('El campo ' + input + ' tiene caracteres invalidos, solo letras y mayor a 4 caracteres');
+        return false;
+    }
+}
+
+function validatePhone(string) {
+
+    var regex = /^[\(]?[\+]?(\d{2}|\d{3})[\)]?[\s]?((\d{6}|\d{8})|(\d{3}[\*\.\-\s]){2}\d{3}|(\d{2}[\*\.\-\s]){3}\d{2}|(\d{4}[\*\.\-\s]){1}\d{4})|\d{8}|\d{10}|\d{12}$/;
+
+    if (regex.test(string)) {
+        return true;
+
+    } else {
+        alert('El campo telefono deberia ser un telefono valido, ejemplo +56 90000000');
+        return false;
+    }
+}
+
+
+
+$(document).ready(function () {
+    $('#check-form-input-1').click(function () {
+        if ($('#check-form-input-1').prop("checked") == true) {
+            checkAllInputs();
+        } else if ($('#check-form-input-1').prop("checked") == false) {
+            uncheckAllInputs();
+        }
+    });
+
+
+    $(window).resize(function () {
+
+        var width = $(window).width();
+        if (width > 769) {
+            $("#button-box-header-box").addClass("visibility-contact-none");
+            $("#block-contact-3").addClass("input-group");
+            animationScrollLeft('#block-contact-3');
+        } 
+    })
+
+});
+
+function checkAllInputs() {
+
+    $('#check-form-input-2').prop('checked', true);
+    $('#check-form-input-3').prop('checked', true);
+    $('#check-form-input-4').prop('checked', true);
+    $('#check-form-input-5').prop('checked', true);
+    $('#check-form-input-6').prop('checked', true);
+    $('#check-form-input-7').prop('checked', true);
+    $('#check-form-input-8').prop('checked', true);
+    $('#check-form-input-9').prop('checked', true);
+    $('#check-form-input-10').prop('checked', true);
+
+}
+
+function uncheckAllInputs() {
+
+    $('#check-form-input-2').prop('checked', false);
+    $('#check-form-input-3').prop('checked', false);
+    $('#check-form-input-4').prop('checked', false);
+    $('#check-form-input-5').prop('checked', false);
+    $('#check-form-input-6').prop('checked', false);
+    $('#check-form-input-7').prop('checked', false);
+    $('#check-form-input-8').prop('checked', false);
+    $('#check-form-input-9').prop('checked', false);
+    $('#check-form-input-10').prop('checked', false);
+
+}
